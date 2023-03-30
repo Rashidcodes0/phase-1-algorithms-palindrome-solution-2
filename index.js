f5e595847a4d9a8dd21742618a8b8e87c4c63044
Abdirashidnoor <abdirashid16112@gmail.com>
2023-03-30 08:15:16 +0300

    assingment done

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  *iterate from the beginning of the string to the middle of the string
+  *compare the letter we're iterating over to the corresponding letter at the end of the string
+ *if the letters don't match, return false
+*if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+
+  The isPalindrome function checks if a given string is a palindrome by comparing characters from both ends of the string towards the middle. 
+  It returns false as soon as a mismatch is found, or true if all characters match. 
+  This algorithm is efficient as it only needs to check half of the characters in the string.
+
 */
 
 // You can run `node index.js` to view these console logs
